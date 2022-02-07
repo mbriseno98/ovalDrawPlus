8b57b46865321219eb58b43689457f77bb0fc630
mbriseno98 <91359741+mbriseno98@users.noreply.github.com>
2022-02-07 00:38:34 -0600

    Add files via upload

---
+++ ovalDrawPlus.java
@@ -0,0 +1,56 @@
+package drawing;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Container; 
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class ovalDrawPlus extends JPanel {
+	
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	    this.setBackground(Color.BLACK);
+	    int panelWidth = getWidth();
+        int panelHeight = getHeight();
+	   
+	g.setColor(Color.BLUE);
+	g.fillRect(0,0,panelWidth,panelHeight);
+	
+	 g.setColor(Color.YELLOW);
+	 g.fillOval(0,0,panelWidth,panelHeight);
+	//.fill.setBackground(Color.BLACK);
+		
+}
+
+	/*public void paintComponent(Graphics g){
+		 super.paintComponent(g);
+		 this.setBackground(Color.BLACK);
+
+		 g.setColor(Color.BLUE);
+		 g.fillRect(75, 10, 350, 340);
+		 
+		 g.setColor(Color.YELLOW);
+		 g.fillOval(1,2,482,357);
+		 */
+
+	public static void main(String[] args) {
+		System.out.println("Oval Draw Plus Starting...");
+		JFrame f = new JFrame ("Oval Draw Plus");
+		//JLabel label = new JLabel("Hello JLabel Centered",JLabel.CENTER); 
+		f.setLocation(250, 150);
+
+		f.add(label);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ovalDrawPlus p = new ovalDrawPlus();
+		f.add(p);
+		f.setSize(400,500);
+		f.setVisible(true);
+
+	}
+
+}
